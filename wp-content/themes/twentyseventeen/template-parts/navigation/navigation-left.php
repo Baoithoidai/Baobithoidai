39bc4a832edffa4b8dba6b33b7ffbbf808198a98
henryviet <lvhung2007@gmail.com>
2018-02-09 20:32:15 -0800

    a

--- wp-content/themes/twentyseventeen/template-parts/navigation/navigation-left.php
+++ wp-content/themes/twentyseventeen/template-parts/navigation/navigation-left.php
@@ -1,13 +1,30 @@
 <div>
 	<div class="row">
 		<div class="col-md-12 nav-top-left-logo">
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Bao Bì Thời Đại">
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-md-12">
-			menu
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png">
+			</a>
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/home.png">
+			</a>
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/intro.png">
+			</a>
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/product.png">
+			</a>
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/knowledge.png">
+			</a>
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact.png">
+			</a>
 		</div>
 	</div>
 

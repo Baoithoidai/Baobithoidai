fc8bfb33ab128ff3f1a0629cc402c3b2d0da53bb
henryviet <lvhung2007@gmail.com>
2018-02-09 20:33:15 -0800

    a

--- wp-content/themes/twentyseventeen/template-parts/navigation/navigation-left.php
+++ wp-content/themes/twentyseventeen/template-parts/navigation/navigation-left.php
@@ -8,9 +8,6 @@
 	<div class="row">
 		<div class="col-md-12">
 			<a href="#">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png">
-			</a>
-			<a href="#">
 				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/home.png">
 			</a>
 			<a href="#">

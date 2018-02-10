9cf6df721cf79a393cc28c214a169c3a15e2ef7a
henryviet <lvhung2007@gmail.com>
2018-02-10 09:32:18 -0800

    a

--- wp-content/themes/twentyseventeen/footer.php
+++ wp-content/themes/twentyseventeen/footer.php
@@ -104,7 +104,7 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
 
 
   $('#carouselExample').carousel({ 
-                interval: 2000
+                interval: 200
         });
 
 

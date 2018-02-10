84ef68719d63b37e3c237bbc0192f2d9aee9ac7a
henryviet <lvhung2007@gmail.com>
2018-02-09 20:47:37 -0800

    a

--- wp-content/themes/twentyseventeen/template-parts/navigation/navigation-top.php
+++ wp-content/themes/twentyseventeen/template-parts/navigation/navigation-top.php
@@ -1,29 +1,18 @@
-<?php
-/**
- * Displays top navigation
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.2
- */
-
-?>
-<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentyseventeen' ); ?>">
-	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
-		<?php
-		echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) );
-		echo twentyseventeen_get_svg( array( 'icon' => 'close' ) );
-		_e( 'Menu', 'twentyseventeen' );
-		?>
-	</button>
-
-	<?php wp_nav_menu( array(
-		'theme_location' => 'top',
-		'menu_id'        => 'top-menu',
-	) ); ?>
-
-	<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
-		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
-	<?php endif; ?>
-</nav><!-- #site-navigation -->
+<div class="row">
+	<div class="col-md-6">
+		<ul>
+			<li>TƯ VẤN</li>
+			<li>THIẾT KẾ</li>
+			<li>IN ẤN BAO BÌ</li>
+		</ul>
+	</div>
+	<div class="col-md-6">
+		<div class="input-group mb-3">
+			  <input type="text" class="form-control" placeholder="Nhập từ khoá tìm kiếm" aria-label="" aria-describedby="basic-addon2">
+			  <div class="input-group-append">
+			  <button class="btn btn-outline-secondary" type="button">Tìm Kiếm</button>
+  		</div>
+</div>
+		
+	</div>
+</div>

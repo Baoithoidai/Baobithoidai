baacd9d566360e886a49b01ab95494b4de90b542
henryviet <lvhung2007@gmail.com>
2018-02-09 19:42:40 -0800

    1

--- wp-content/themes/twentyseventeen/index.php
+++ wp-content/themes/twentyseventeen/index.php
@@ -16,19 +16,7 @@
  */
 
 get_header(); ?>
-
-<div class="wrap">
-	<?php if ( is_home() && ! is_front_page() ) : ?>
-		<header class="page-header">
-			<h1 class="page-title"><?php single_post_title(); ?></h1>
-		</header>
-	<?php else : ?>
-	<header class="page-header">
-		<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
-	</header>
-	<?php endif; ?>
-
-	<div class="container">
+<div class="container">
 		<div class="row">
 			<div class="col-md-2">
 				test cot ben trai
@@ -39,5 +27,4 @@ get_header(); ?>
 		</div>
 		
 	</div>
-
 <?php get_footer();

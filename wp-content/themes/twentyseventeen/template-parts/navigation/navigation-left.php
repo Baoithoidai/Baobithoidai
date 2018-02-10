b38b401fdac593bf5c71e3c38ae9eb55f1137568
henryviet <lvhung2007@gmail.com>
2018-02-09 20:01:23 -0800

    a

--- wp-content/themes/twentyseventeen/template-parts/navigation/navigation-left.php
+++ wp-content/themes/twentyseventeen/template-parts/navigation/navigation-left.php
@@ -2,18 +2,18 @@
 	<div class="row">
 		<div class="col-md-12">
 			logo
-		</div>>
+		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-md-12">
 			menu
-		</div>>
+		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-md-12">
 			social
-		</div>>
+		</div>
 	</div>
 </div>

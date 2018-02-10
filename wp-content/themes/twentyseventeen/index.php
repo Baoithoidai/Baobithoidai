7f0c703722e9136fbab0e6b7fc67b9e96610b1ad
henryviet <lvhung2007@gmail.com>
2018-02-09 22:11:55 -0800

    q

--- wp-content/themes/twentyseventeen/index.php
+++ wp-content/themes/twentyseventeen/index.php
@@ -1,33 +1,32 @@
 <?php
 /**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
- */
-
+* The main template file
+*
+* This is the most generic template file in a WordPress theme
+* and one of the two required files for a theme (the other being style.css).
+* It is used to display a page when nothing more specific matches a query.
+* E.g., it puts together the home page when no home.php file exists.
+*
+* @link https://codex.wordpress.org/Template_Hierarchy
+*
+* @package WordPress
+* @subpackage Twenty_Seventeen
+* @since 1.0
+* @version 1.0
+*/
 get_header(); ?>
 <div class="container">
-		<div class="row">
-			<div class="col-md-2">
-				<?php get_template_part( 'template-parts/navigation/navigation', 'left' ); ?>
-			</div>
-			<div class="col-md-10">
-				<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
-				<?php get_template_part( 'template-parts/slider/banner', 'top' ); ?>
-				<?php get_template_part( 'template-parts/content/content', 'top' ); ?>
-
-			</div>
+	<div class="row">
+		<div class="col-md-2">
+			<?php get_template_part( 'template-parts/navigation/navigation', 'left' ); ?>
 		</div>
-		
+		<div class="col-md-10">
+			<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
+			<?php get_template_part( 'template-parts/slider/banner', 'top' ); ?>
+			<?php get_template_part( 'template-parts/content/content', 'top' ); ?>
+			<?php get_template_part( 'template-parts/content/content', 'middle' ); ?>
+		</div>
 	</div>
+	
+</div>
 <?php get_footer();
---
+++ wp-content/themes/twentyseventeen/template-parts/content/content-middle.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-md-5">
+        <img src="...." alt="..">
+    </div>
+    <div class="col-md-7">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <a href="#" class="btn float-right">View More</a>
+            </div>
+        </div>
+    </div>
+</div>

e961357eeeb2078831af6ef669cafd06e277f452
henryviet <lvhung2007@gmail.com>
2018-02-11 08:19:25 -0800

    a

--- wp-content/themes/twentyseventeen/template-parts/footer/footer-top.php
+++ wp-content/themes/twentyseventeen/template-parts/footer/footer-top.php
@@ -1,47 +1,25 @@
 <div class="row mt-4">
     <div class="col-md-5">
-        
+        aaaaaaaaaaaaa
+        <p class="copyright float-left">© 2016 | BAO BI THOI DAI JSC | All rights reserved</p>
     </div>
-
     <div class="col-md-5">
-<ul class="footer-menu float-right">
-<li>
-<a href="http://baobithoidai.com/">Trang chủ</a>
-</li>
-<li>
-<a href="http://baobithoidai.com/gioi-thieu.html">Giới thiệu</a>
-</li>
-<li>
-<a href="http://baobithoidai.com/san-pham.html">Sản phẩm</a>
-</li>
-<li>
-<a href="http://baobithoidai.com/kien-thuc.html">Kiến thức</a>
-</li>
-<li class="f-li-last">
-<a href="http://baobithoidai.com/lien-he.html">Liên hệ</a>
-</li>
-</ul>
+        <ul class="footer-menu float-right">
+            <li>
+                <a href="http://baobithoidai.com/">Trang chủ</a>
+            </li>
+            <li>
+                <a href="http://baobithoidai.com/gioi-thieu.html">Giới thiệu</a>
+            </li>
+            <li>
+                <a href="http://baobithoidai.com/san-pham.html">Sản phẩm</a>
+            </li>
+            <li>
+                <a href="http://baobithoidai.com/kien-thuc.html">Kiến thức</a>
+            </li>
+            <li class="f-li-last">
+                <a href="http://baobithoidai.com/lien-he.html">Liên hệ</a>
+            </li>
+        </ul>
     </div>
 </div>
-
-
-<p class="copyright float-left">© 2016 | BAO BI THOI DAI JSC | All rights reserved</p>
-<ul class="footer-menu float-right">
-<li>
-<a href="http://baobithoidai.com/">Trang chủ</a>
-</li>
-<li>
-<a href="http://baobithoidai.com/gioi-thieu.html">Giới thiệu</a>
-</li>
-<li>
-<a href="http://baobithoidai.com/san-pham.html">Sản phẩm</a>
-</li>
-<li>
-<a href="http://baobithoidai.com/kien-thuc.html">Kiến thức</a>
-</li>
-<li class="f-li-last">
-<a href="http://baobithoidai.com/lien-he.html">Liên hệ</a>
-</li>
-<div class="clear"></div>
-</ul>
-<div class="clear"></div>

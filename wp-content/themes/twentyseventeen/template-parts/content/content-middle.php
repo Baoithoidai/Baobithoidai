903d97364d8c73acde971ab9305bb872892e5e56
henryviet <lvhung2007@gmail.com>
2018-02-11 09:15:32 -0800

    a

--- wp-content/themes/twentyseventeen/template-parts/content/content-middle.php
+++ wp-content/themes/twentyseventeen/template-parts/content/content-middle.php
@@ -1,12 +1,12 @@
 <div class="row mt-4">
     <div class="col-md-5">
-        <img src="...." alt="..">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Insoluongittheoyeucau.jpg" alt="In So Luong It Theo Yeu Cau">
     </div>
     <div class="col-md-7">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title">In Số Lượng Ít Theo Yêu Cầu</h5>
+                <p class="card-text">Nhằm đáp ứng nhu cầu in ấn phẩm số lượng ít với chất lượng cao của khách hàng, Công ty Bao Bì Thời Đại cho phát triển thêm dịch vụ in số lượng ít theo yêu cầu. Đây là một lựa chọn hiệu quả cho các đơn hàng số lượng ít với chất lượng vượt trội. Dịch vụ này còn có ưu điểm là  giao hàng nhanh đúng số lượng cần thiết, với chất lượng ổn định từ tờ in đầu tiên cho đến tờ in cuối cùng sẽ đáp ứng nhu cầu kinh doanh của Quý khách.</p>
                <a href="#" class="btn float-right">View More</a>
             </div>
         </div>
--- wp-content/themes/twentyseventeen/template-parts/content/content-top.php
+++ wp-content/themes/twentyseventeen/template-parts/content/content-top.php
@@ -11,21 +11,21 @@
 	</div>
 	<div class="col-md-4">
 			<div class="card">
-	  		<img class="card-img-top" src="..." alt="Card image cap">
+	  		<img class="card-img-top" src="<?php echo get_template_directory_uri(); ?>/assets/images/Inbaobigiay.png" alt="In Bao Bì Giấy">
 	 		 <div class="card-body">
-	    	<h5 class="card-title">In Hộp Giấy</h5>
-	    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-	    	<a href="#" class="btn float-right">View More</a>
+	    	<h5 class="card-title">In Bao Bì Giấy</h5>
+	    	<p class="card-text">Bao bì là một phần không thể thiếu của một sản phẩm. Bao bì không chỉ có tác dụng bảo vệ sản phẩm mà nó còn mô tả và giới thiệu sản phẩm đến khách hàng. Bao bì thường có bốn yếu tố cầu thành điển hình: Lớp tiếp xúc trực tiếp với sản phẩm, lớp bảo vệ lớp tiếp xúc ...</p>
+	    	<a href="#" class="btn float-right">ĐỌC THÊM</a>
   </div>
 </div>
 	</div>
 	<div class="col-md-4">
 			<div class="card">
-	  		<img class="card-img-top" src="..." alt="Card image cap">
+	  		<img class="card-img-top" src="<?php echo get_template_directory_uri(); ?>/assets/images/Inhopgiay.png" alt="In Hộp Giấy">
 	 		 <div class="card-body">
-	    	<h5 class="card-title">In Bao Bì Giấy</h5>
-	    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-	    	<a href="#" class="btn float-right">View More</a>
+	    	<h5 class="card-title">In Hộp Giấy</h5>
+	    	<p class="card-text">In hộp giấy là một trong những hình thức kích thích tiêu dùng hiệu quả trong mọi chiến dịch marketing. Hộp giấy, tem nhãn, bao bì vừa giúp doanh nghiệp định vị thương hiệu, vừa tôn lên chất lượng sản phẩm và làm cho sản phẩm bắt mắt hơn với người tiêu dùng ...</p>
+	    	<a href="#" class="btn float-right">ĐỌC THÊM</a>
   </div>
 </div>
 	</div>

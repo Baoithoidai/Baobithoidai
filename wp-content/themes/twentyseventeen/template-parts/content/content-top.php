8ade5239ae8397467bcca90e98e3cc3f287acd0d
henryviet <lvhung2007@gmail.com>
2018-02-11 08:38:19 -0800

    A

--- wp-content/themes/twentyseventeen/template-parts/content/content-top.php
+++ wp-content/themes/twentyseventeen/template-parts/content/content-top.php
@@ -1,9 +1,19 @@
 <div class="row mt-4">
 	<div class="col-md-4">
 			<div class="card">
+	  		<img class="card-img-top" src="<?php echo get_template_directory_uri(); ?>/assets/images/Inoffset.png" alt="In Offset">
+	 		 <div class="card-body">
+	    	<h5 class="card-title">In Offset</h5>
+	    	<p class="card-text">In Offset được xem là một kỹ thuật in ấn phổ biến nhất hiện nay, giúp khách hàng tiết kiệm chi phí và thời gian in ấn. Các chi tiết in ấn được ép lên một tấm cao su, sau đó mới từ tấm cao su này ép lên bề mặt giấy in. Vì vậy bản thường chính xác, đẹp và ổn định ...</p>
+	    	<a href="#" class="btn float-right">ĐỌC THÊM</a>
+  </div>
+</div>
+	</div>
+	<div class="col-md-4">
+			<div class="card">
 	  		<img class="card-img-top" src="..." alt="Card image cap">
 	 		 <div class="card-body">
-	    	<h5 class="card-title">Card title</h5>
+	    	<h5 class="card-title">In Hộp Giấy</h5>
 	    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 	    	<a href="#" class="btn float-right">View More</a>
   </div>
@@ -13,17 +23,7 @@
 			<div class="card">
 	  		<img class="card-img-top" src="..." alt="Card image cap">
 	 		 <div class="card-body">
-	    	<h5 class="card-title">Card title</h5>
-	    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-	    	<a href="#" class="btn float-right">View More</a>
-  </div>
-</div>
-	</div>
-	<div class="col-md-4">
-			<div class="card">
-	  		<img class="card-img-top" src="..." alt="Card image cap">
-	 		 <div class="card-body">
-	    	<h5 class="card-title">Card title</h5>
+	    	<h5 class="card-title">In Bao Bì Giấy</h5>
 	    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 	    	<a href="#" class="btn float-right">View More</a>
   </div>
